a7cbde181ce8b1a166ae69c718608c8cffdc10a1
SivakulaRajalaxmi <samiraverma2003@gmail.com>
2026-02-22 20:14:09 +0530

    changes made to timetableviewer.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TimetableViewer from './components/TimetableViewer'
 
-import UserSelectionForm from "./components/UserSelectionForm";
-
-function App() {
-  return (
-    <div>
-      <UserSelectionForm />
-    </div>
-  );
+export default function App() {
+  return <TimetableViewer />
 }
-
-export default App;
 
  
     
