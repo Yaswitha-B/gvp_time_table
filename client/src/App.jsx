425765f8ff8cea25610d68664900e8f9a6ad4578
SivakulaRajalaxmi <samiraverma2003@gmail.com>
2026-02-22 20:29:48 +0530

    merge conflict resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,4 @@
 import TimetableViewer from './components/TimetableViewer'
-<<<<<<< HEAD
-
-export default function App() {
-  return <TimetableViewer />
-}
-
- 
-    
-=======
->>>>>>> 63e239189352a63af3abaf26cdf0494d327df840
 
 export default function App() {
   return <TimetableViewer />
